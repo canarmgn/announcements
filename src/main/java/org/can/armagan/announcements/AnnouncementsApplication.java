b6d2b0e1b5369e58d7a7abcb70c1282dc9254429
canarmagan <canarmgn@gmail.com>
2024-05-29 12:09:24 +0300

    .

---
+++ src/main/java/org/can/armagan/announcements/AnnouncementsApplication.java
@@ -0,0 +1,13 @@
+package org.can.armagan.announcements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnouncementsApplication.class, args);
+    }
+
+}
